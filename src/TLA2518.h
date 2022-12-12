f19da0c018ba524c33919bed38b43f226961f738
dlyckelid <79441959+dlyckelid@users.noreply.github.com>
2022-12-12 13:48:45 +0100

    Update TLA2518.h

--- src/TLA2518.h
+++ src/TLA2518.h
@@ -1,8 +1,20 @@
 /*
- Name:    TLA2518.h
- Created: 11/28/2022 8:55:21 AM
- Author:  David
- Editor:  http://www.visualmicro.com
+  This file is part of the IOExpander-TLA2518 library.
+  Copyright (c) 2022 Good Solutions Sweden AB. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #pragma once
